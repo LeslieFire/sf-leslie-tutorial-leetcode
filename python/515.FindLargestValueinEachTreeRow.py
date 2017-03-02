8b683610ba1e24fa567ca76a966ead32fdd8138f
Leslie <249893977@qq.com>
2017-03-02 13:44:02 +0800

    multi python complete

--- python/515.FindLargestValueinEachTreeRow.py
+++ python/515.FindLargestValueinEachTreeRow.py
@@ -1,3 +1,37 @@
+##########BFS--- 72ms#################
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def largestValues(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        maxes = []
+        row = [root]
+        while any(row):
+            maxes.append(max(node.val for node in row))
+            row = [kid for node in row for kid in (node.left, node.right) if kid]
+        return maxes
+
+
+#########DFS--- 222ms####################
+class Solution(object):
+    def largestValues(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        
+        return [root.val] + map(max, *map(self.largestValues, (root.left, root.right))) if root else []
+
+#########DFS--- 95ms#####################
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
